ffc10b51d89095ad1fcfc936800c935c8237c616
Marshall Whittaker <marshall@oxasploits.com>
2025-08-29 15:21:12 -0400

    Rebasing.

--- Starter.py
+++ Starter.py
@@ -12,12 +12,10 @@ GUILD_ID = botconfig.getint('API', 'guildid')
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 tree = discord.app_commands.CommandTree(client)
-
 @client.event
 async def on_ready():
     print(f"Bot is ready. Logged in as {client.user} (ID: {client.user.id})")
     await tree.sync(guild=discord.Object(id=GUILD_ID))
-
 # Commands
 @tree.command(name="ping", description="sends ping of bot", guild=discord.Object(id=GUILD_ID))
 async def ping(interaction: discord.Interaction):
@@ -25,4 +23,3 @@ async def ping(interaction: discord.Interaction):
     await interaction.response.send_message(f'Pong! `{latency:.2f}ms`', ephemeral=True)
 
 client.run(BOT_TOKEN)
-
