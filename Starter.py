867590641eb3a211aaa395cd9a75485b1af9c120
Thalia Blair <holden.blairpeace17@gmail.com>
2025-10-18 13:30:21 -0400

    Sync commands to a specific guild

--- Starter.py
+++ Starter.py
@@ -5,6 +5,7 @@ from discord import app_commands, Interaction, Embed
 
 # Setup Credentials
 BOT_TOKEN = 'Bot_Token_Here' # Replace With Bot Token
+GUILD_ID = 12345678912345 # Replace With Your Sever ID
 
 # Setup
 intents = discord.Intents.all()
@@ -14,13 +15,10 @@ tree = discord.app_commands.CommandTree(client)
 @client.event
 async def on_ready():
     print(f"Bot is ready. Logged in as {client.user} (ID: {client.user.id})")
-    await tree.sync()
-    for guild in client.guilds:
-        print(f"Bot added to guild {guild.name} (ID: {guild.id})")
-    print(f'Bot is in {len(client.guilds)} guilds')
+    await tree.sync(guild=discord.Object(id=GUILD_ID))
 
 # Commands
-@tree.command(name="ping", description="sends ping of bot")
+@tree.command(name="ping", description="sends ping of bot", guild=discord.Object(id=GUILD_ID))
 async def ping(interaction: discord.Interaction):
     latency = client.latency * 1000  # Convert to ms
     await interaction.response.send_message(f'Pong! `{latency:.2f}ms`', ephemeral=True)
